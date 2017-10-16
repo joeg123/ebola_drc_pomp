bfbf88e71bee17f8f1b64302697f2a1a632c129d
Joseph Garcia <Joseph@wireless-10-146-109-145.public.utexas.edu>
2017-10-16 14:08:41 -0500

    code with simulated fitting

--- R/ebola_outbreak_althaus_model.R
+++ R/ebola_outbreak_althaus_model.R
@@ -78,6 +78,11 @@ seir_dmeasure <- Csnippet('double f;
                           f = dpois(nearbyint(cases), C,1);
                           lik = (give_log) ? f : exp(f);
                           ')
+
+seir_rmeasure <- Csnippet('
+                          cases = rpois(C);
+                          ')
+
 trans <- Csnippet('
                     Tbeta0 = log(beta0);
                     Tbeta1 = log(beta1);
@@ -108,6 +113,13 @@ init <- Csnippet("
           C = 0.0;
                  ")
 
+seir.init.state <- c(S.0= 999999, 
+                     E.0 = 0.0,
+                     I.0 = 1.0,
+                     R.0 = 0.0,
+                     D.0 = 0.0,
+                     C.0 = 0.0)
+
 seir_parm <- c(sigma = 1/9.312799, 
                gamma = 1/7.411374, 
                ff = plogis(49/69),
@@ -129,6 +141,7 @@ pomp(data = data,
   fromEstimationScale = untrans,
   toEstimationScale = trans,
   dmeasure = seir_dmeasure,
+  rmeasure = seir_rmeasure,
   rprocess = euler.sim(step.fun = seir_rprocess, delta.t = 1)) -> seir_pomp
 
 #trajectory matching
@@ -145,6 +158,212 @@ t_match$params
 # Should match Althaus results
 #!beta0!      beta1          !k!          f       tau0       !tau1!      sigma      gamma 
 #-0.3634729       -Inf -2.0883390  0.8960880       -Inf 14.2890158  0.1073791  0.1349277
+
+#####
+# Log lik profile
+#####
+
+sliceDesign(
+  center=c(beta0 = .7454, tau1 = 12.8366, k=.1249,sigma = 1/9.312799, 
+           gamma = 1/7.411374, ff = plogis(49/69), beta1 = 1),
+  beta0=rep(seq(from=0.01,to=2.0,length=20),each=10),
+  tau1=rep(seq(from=9,to=30,length=20),each=10),
+  k=rep(seq(from=.01,to=.30,length=20),each=10)
+) -> p
+
+registerDoParallel()
+set.seed(108028909,kind="L'Ecuyer")
+
+foreach (theta=iter(p,"row"),.combine = rbind,
+         .inorder = FALSE,
+         .options.multicore=list(set.seed=TRUE)
+) %dopar% {
+           library (pomp)
+  pfilter(seir_pomp,params=unlist(theta),Np=500) -> pf
+  theta$loglik <- logLik(pf)
+  theta
+  } -> p
+
+
+p %>% 
+  gather(parm, value, beta0, tau1, k) %>%
+  filter(slice == parm) %>%
+  ggplot(aes(x=value,y=loglik, color=parm))+
+  geom_point()+
+  facet_wrap(~parm, scales = "free_x") +
+  geom_smooth()
+
+#########
+#End log lik profile
+########
+
+########
+# Testing pomp mode
+########
+
+
+#Trajectory 
+SEIR.traj <- trajectory(seir_pomp, params = c(t_match$params, seir.init.state), as.data.frame=TRUE)
+
+# Create synthetic data
+
+synth_data <- (SEIR.traj[c("time","C")])
+#dC <- c(diff(SEIR.traj$C))
+dC <- round(SEIR.traj$C)
+#dC <- c(0, dC)
+synth_data$C <- c(dC)
+names(synth_data) <- c("times","cases")
+
+
+#Deterministic
+
+pomp(data = synth_data,
+     times="times",
+     t0=0,
+     skeleton = vectorfield(seir_skel), 
+     params = seir_parm,
+     initializer=init,
+     statenames= c("S","E","I","R", "D", "C"), 
+     paramnames=c("tau1", "beta0","k", "sigma", "gamma", "ff","beta1"),
+     zeronames = c("C"),
+     fromEstimationScale = untrans,
+     toEstimationScale = trans,
+     dmeasure = seir_dmeasure,
+     rmeasure = seir_rmeasure,
+     rprocess = euler.sim(step.fun = seir_rprocess, delta.t = 1)) -> seir_pomp_test
+
+traj.match(seir_pomp_test, 
+           start=seir_parm,
+           method = c("Nelder-Mead"),
+           est=c("tau1", "beta0","k"),
+           transform=TRUE) -> t_match_test
+
+t_match_test$params
+trajectory(seir_pomp_test, params=t_match_test$params, times=0:120, as.data.frame=TRUE) %>% 
+  ggplot(aes(x=time, y=C)) + geom_line() +
+  geom_point(data=synth_data, aes(x=times, y = cases), color = "red")
+
+# Simulate using new rmeasure
+
+sim <- simulate(seir_pomp, params = c(t_match_test$params, seir.init.state), nsim = 10)
+
+# no clue how to graph this
+
+
+
+#       sigma        gamma           ff         tau1        beta0        beta1            k 
+#1.073791e-01 1.349277e-01 6.704332e-01 3.535753e+01 3.131401e-01 1.000000e+00 3.212365e-09 
+#
+
+#log lik of simulated data  
+
+sliceDesign(
+  center=c(beta0 = .7454, tau1 = 12.8366, k=.1249,sigma = 1/9.312799, 
+           gamma = 1/7.411374, ff = plogis(49/69), beta1 = 1),
+  beta0=rep(seq(from=0.01,to=2.0,length=20),each=10),
+  tau1=rep(seq(from=9,to=30,length=20),each=10),
+  k=rep(seq(from=.01,to=.30,length=20),each=10)
+) -> p
+
+registerDoParallel()
+set.seed(108028909,kind="L'Ecuyer")
+
+foreach (theta=iter(p,"row"),.combine = rbind,
+         .inorder = FALSE,
+         .options.multicore=list(set.seed=TRUE)
+) %dopar% {
+  library (pomp)
+  pfilter(seir_pomp_test,params=unlist(theta),Np=500) -> pf_test
+  theta$loglik <- logLik(pf_test)
+  theta
+} -> p_t
+
+
+p_t %>% 
+  gather(parm, value, beta0, tau1, k) %>%
+  filter(slice == parm) %>%
+  ggplot(aes(x=value,y=loglik, color=parm))+
+  geom_point()+
+  facet_wrap(~parm, scales = "free_x") +
+  geom_smooth()
+
+
+## Maximizing likelihood using pfilter
+# pfilter give stochastic est of liklihood
+# must use derivative free algorithm to 
+# optimizing over unbounded parms (must transform)
+
+# fixing a reference point in parameter space
+neg.ll <- function (par, est) {
+  allpars <- coef(flu,transform=TRUE)
+  allpars[est] <- par
+  try(
+    freeze(
+      pfilter(flu,params=partrans(flu,allpars,dir="fromEst"),
+              Np=2000),
+      seed=915909831
+    )
+  ) -> pf
+  if (inherits(pf,"try-error")) 1e10 else -logLik(pf)
+}
+
+# Nelder mead optimizes function (neg.ll)
+fit <- optim(
+  par=c(),
+  est=c(),
+  fn=negloglik,
+  method="Nelder-Mead",
+  control=list(maxit=400,trace=0)
+)
+
+mle <- flu
+coef(mle,c("Beta","mu_I","rho"),transform=TRUE) <- fit$par
+coef(mle)
+
+
+
+###############
+# End of testing
+################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 times <- 1:100
 rnot <- if_else(times<t_match$params["tau1"], 
                 t_match$params["beta0"]/t_match$params["gamma"], 
@@ -156,45 +375,11 @@ seir_parm["k"] = t_match$params["k"]
 seir_parm["tau1"] = t_match$params["tau1"]
 seir_parm["beta0"] = t_match$params["beta0"]
 
+trajectory(pomp(seir_pomp, params = seir_parm), as.data.frame=TRUE)
+
+
 pfilter(seir_pomp, params=seir_parm, Np=1000, filter.mean=TRUE) -> test
 logLik(test)
-
-#####
-# Log lik profile
-#####
-
-sliceDesign(
-  center=c(beta0 = .75, tau1 = 12.8, k=.12,sigma = 1/9.312799, 
-           gamma = 1/7.411374, ff = plogis(49/69), beta1 = 1),
-  beta0=rep(seq(from=0.7,to=.8,length=10),each=3),
-  tau1=rep(seq(from=12,to=13.5,length=10),each=3),
-  k=rep(seq(from=.09,to=.15,length=10),each=3)
-) -> p
-
-registerDoParallel()
-set.seed(108028909,kind="L'Ecuyer")
-
-foreach (theta=iter(p,"row"),.combine = rbind,
-         .inorder = FALSE,
-         .options.multicore=list(set.seed=TRUE)
-) %dopar% {
-           library (pomp)
-  pfilter(seir_pomp,params=unlist(theta),Np=5000) -> pf
-  theta$loglik <- logLik(pf)
-  theta
-  } -> p
-
-library(reshape2)
-p %>% melt(measure = c("beta0", "k", "tau1")) %>%
-  # subset(variable == "slice") %>%
-  ggplot(aes(x=value,y=loglik, color=variable))+
-  geom_point()+
-  facet_wrap(~variable, scales = "free_x")
-
-#########
-#End log lik profile
-########
-
 
 #################################################
 ## Pfilter tester
